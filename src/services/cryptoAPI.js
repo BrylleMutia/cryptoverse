6c47b3da412db481cfa5af0f6b893754ac3c0385
Brylle Mutia <bryllemutia3@gmail.com>
2021-11-06 21:44:47 +0800

    added first api call for statistics on homepage

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'd1a8853320mshe97cd23d9d2d162p1decf7jsnd84a74dea553'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+})
+
+// RTK automatically creates this hook based on the endpoints created on createApi function
+export const { useGetCryptosQuery } = cryptoApi;
