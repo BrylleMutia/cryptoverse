d2ece81b03daf2efa79673c25fe53a1883471d9e
Brylle Mutia <bryllemutia3@gmail.com>
2021-11-06 23:16:33 +0800

    added news section

---
+++ src/services/cryptoNewsAPI.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': 'd1a8853320mshe97cd23d9d2d162p1decf7jsnd84a74dea553'
+}
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      // params added to query hook is accessible here
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+    })
+  })
+})
+
+// RTK automatically creates this hook based on the endpoints created on createApi function
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
